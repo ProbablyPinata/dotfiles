e7d6c3c2fea1b22d66e8d36e3222439a2150e522
Stefan van Biljon <stefan@stefans-mba.local>
2024-09-14 21:47:34 +0200

    treesitter: new parsers, disable indent

--- .config/nvim/lua/pinata/plugins/treesitter.lua
+++ .config/nvim/lua/pinata/plugins/treesitter.lua
@@ -8,10 +8,10 @@ return {
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "html", "haskell", "bash", "kotlin", "cpp"},
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "html", "haskell", "bash", "kotlin", "cpp", "toml", "latex", "markdown", "markdown_inline" },
                 sync_install = false,
                 highlight = { enable = true },
-                indent = { enable = true },
+                indent = { enable = false }, -- buggy, and seems to conflic with nvim-autopairs
             })
         end
     },
