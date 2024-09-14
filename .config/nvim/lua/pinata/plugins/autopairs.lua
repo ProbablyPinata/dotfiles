fceaefe6d879772706d076a48b3a95e16a85c744
Stefan van Biljon <stefan@stefans-mba.local>
2024-09-14 21:18:09 +0200

    add treesitter dependency

--- .config/nvim/lua/pinata/plugins/autopairs.lua
+++ .config/nvim/lua/pinata/plugins/autopairs.lua
@@ -1,5 +1,12 @@
 return {
     'windwp/nvim-autopairs',
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    },
     event = "InsertEnter",
-    config = true
+    config = function()
+        require("nvim-autopairs").setup({
+            check_ts = true
+        })
+    end
 }
