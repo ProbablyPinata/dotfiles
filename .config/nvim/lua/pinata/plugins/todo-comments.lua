15cc9f03a6f236eed12c9903f0b982bcc3333449
Stefan van Biljon <stefan@stefans-mba.local>
2024-09-14 21:46:31 +0200

    todo-comments: load on BufEnter

--- .config/nvim/lua/pinata/plugins/todo-comments.lua
+++ .config/nvim/lua/pinata/plugins/todo-comments.lua
@@ -2,7 +2,7 @@ return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     lazy = true,
-    event = "BufReadPost",
+    event = "BufEnter",
     opts = {
         -- your configuration comes here
         -- or leave it empty to use the default settings
