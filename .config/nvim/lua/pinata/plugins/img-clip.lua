9c608d380daf2298df067bcf320575d3ba85e6b4
ProbablyPinata <50218482+ProbablyPinata@users.noreply.github.com>
2024-09-15 20:11:12 +0200

    new plugin: img-clip

---
+++ .config/nvim/lua/pinata/plugins/img-clip.lua
@@ -0,0 +1,10 @@
+return {
+    "HakonHarnes/img-clip.nvim",
+    opts = {
+        dir_path = "images",
+    },
+    keys = {
+        -- suggested keymap
+        { "<leader>ip", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+    },
+}
